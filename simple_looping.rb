9d2560de14498fd832031738b59b8b2c34614f3c
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-17 22:21:09 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -31,9 +31,10 @@ end
 end
 
 def for_iterator(number_of_times)
-  number_of_times = 1..7
+  number_of_times = 7
   for number_of_times in 0..7 do
    puts "Welcome to Flatiron School's Web Development Course!"
+   number_of_times -= 1
   end
 end
 
