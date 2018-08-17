dac2368a9050f4fd62b69f3939dc33d393dc98a9
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-17 22:12:42 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -18,7 +18,7 @@ def while_iterator(number_of_times)
   number_of_times = 0
 while number_of_times < 7
   puts = "Welcome to Flatiron School's Web Development Course!"
-   
+   number_of_times += 1
 end
 end
 
